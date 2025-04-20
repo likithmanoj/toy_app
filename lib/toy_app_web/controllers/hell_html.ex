38b9abea1de6c1413c4169958f3b4682fc90c9fc
likithmanoj <likithmnaojm@gmail.com>
2025-04-20 09:18:15 +0530

    hell_form commit

--- lib/toy_app_web/controllers/hell_html.ex
+++ lib/toy_app_web/controllers/hell_html.ex
@@ -1,10 +1,10 @@
-defmodule ToyAppWeb.HellHTML do
-  @moduledoc """
-  This module contains pages rendered by HelpController.
+# defmodule ToyAppWeb.HellHTML do
+#   @moduledoc """
+#   This module contains pages rendered by HelpController.
 
-  See the `page_html` directory for all templates available.
-  """
-  use ToyAppWeb, :html
+#   See the `page_html` directory for all templates available.
+#   """
+#   use ToyAppWeb, :html
 
-  embed_templates "hell_html/*"
-end
+#   embed_templates "hell_html/*"
+# end
